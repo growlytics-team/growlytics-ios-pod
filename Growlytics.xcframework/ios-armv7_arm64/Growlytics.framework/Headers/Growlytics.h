811cdcbbf5ff509a4a367e55430474c7e7df12cc
Harigharan <harigharan.e@gmail.com>
2020-05-09 15:53:30 +0530

    pod 1.0.1 framework release

---
+++ Growlytics.xcframework/ios-armv7_arm64/Growlytics.framework/Headers/Growlytics.h
@@ -0,0 +1,19 @@
+//
+//  Growlytics.h
+//  Growlytics
+//
+//  Created by Pradeep Singh on 04/03/20.
+//  Copyright © 2020 Growlytics Technologies Pvt Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Growlytics.
+FOUNDATION_EXPORT double GrowlyticsVersionNumber;
+
+//! Project version string for Growlytics.
+FOUNDATION_EXPORT const unsigned char GrowlyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Growlytics/PublicHeader.h>
+
+
